9fda27fd2e719277a7ad658467fa8c749298788e
akitoshi-y <aomine6@gmail.com>
2022-09-11 20:41:18 +0900

    追加

--- common/js/script.js
+++ common/js/script.js
@@ -11,6 +11,12 @@ $(function () {
   //swiper
   //--------------------------------------------
   const swiper = new Swiper('.swiper', {
+
+    effect: 'fade', // フェードモードにする（デフォルトは 'slide'）
+    fadeEffect: {
+      crossFade: true, // クロスフェードを有効にする（フェードモードの場合 true 推奨）
+    },
+
     // Optional parameters
     loop: true,
     // If we need pagination
